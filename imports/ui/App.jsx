83ed93f90e48b7e58a53b8cbc8c5854e58e4e37e
Christopher Steven Ciampoli <chrisc@theluxergroup.com>
2021-06-13 06:25:28 -0700

    Adding missing packages

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import { useTracker } from 'meteor/react-meteor-data';
 import _ from 'lodash';
 import { makeStyles } from '@material-ui/core/styles';
@@ -66,23 +72,54 @@ export const App = () => {
   }
 
   return (
-      <Container name="AppContainer" maxWidth="xl">
-          <AppHeader/>
-          <React.Fragment>
-              <CssBaseline />
-                  <Typography width={1} component="div" style={{ backgroundColor: '#cfe8fc'}}>
-                      <Nav>
-                          <div id="navnav" className={classes.root}>
-                              <Paper elevation={0} />
-                              <Paper>
-                                  <Feed />
-                              </Paper>
-                              <Paper elevation={3} />
-                          </div>
-                      </Nav>
-                  </Typography>
-          </React.Fragment>
-          <Feed />
-      </Container>
+      <Router>
+          <div>
+              <nav>
+                  <ul>
+                      <li>
+                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                          <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                          <Link to="/users">Users</Link>
+                      </li>
+                  </ul>
+              </nav>
+
+              {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+              <Switch>
+                  <Route path="/about">
+                      <About />
+                  </Route>
+                  <Route path="/users">
+                      <Users />
+                  </Route>
+                  <Route path="/">
+                      <Home />
+                  </Route>
+              </Switch>
+          </div>
+          <Container name="AppContainer" maxWidth="xl">
+              <AppHeader/>
+              <React.Fragment>
+                  <CssBaseline />
+                      <Typography width={1} component="div" style={{ backgroundColor: '#cfe8fc'}}>
+                          <Nav>
+                              <div id="navnav" className={classes.root}>
+                                  <Paper elevation={0} />
+                                  <Paper>
+                                      <Feed />
+                                  </Paper>
+                                  <Paper elevation={3} />
+                              </div>
+                          </Nav>
+                      </Typography>
+              </React.Fragment>
+              <Feed />
+          </Container>
+      </Router>
   );
 };
